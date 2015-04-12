816bddb84ff18f08b7a81beb3acdc77b36e93381
Mark Steward <marksteward@gmail.com>
2015-04-12 20:18:22 +0000

    Set session cookie to secure
    Thanks to Snader_LB for the prod

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
-Frab::Application.config.session_store :active_record_store, key: '_frab_session'
+Frab::Application.config.session_store :active_record_store,
+                                       key: '_frab_session',
+                                       secure: Rails.env == 'production',
+                                       httponly: true
 
 # Example for a cookie store, with secure flag set for SSL hosting in production mode
 #
